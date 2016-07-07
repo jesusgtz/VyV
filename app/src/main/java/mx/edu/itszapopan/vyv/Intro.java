1535b5e166246978e84da3d7bf9d8bc4f4ab7e0b
Jesus Gutierrez <gusj93@hotmail.com>
2016-07-07 02:11:28 -0500

    Se termina layout de intro y se inicia desarrollo de mainactivity

--- app/src/main/java/mx/edu/itszapopan/vyv/Intro.java
+++ app/src/main/java/mx/edu/itszapopan/vyv/Intro.java
@@ -1,13 +1,30 @@
 package mx.edu.itszapopan.vyv;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intro extends AppCompatActivity {
+
+    Button b;
+    Context c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        c = this;
+        b = (Button) findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(c, MainActivity.class));
+            }
+        });
     }
 }
